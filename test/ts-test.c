53b5b5cad14d5e9b75c1669a9c584ea6b4d2061f
Martin Wilck <mwilck@suse.com>
2021-02-22 19:47:16 +0100

    add ts-test.c and tv-test.c to git repo
    These are auto-generated, but having them in git makes it possible
    to build without GNU "indent".

---
+++ test/ts-test.c
@@ -0,0 +1,103 @@
+/*
+ * Copyright (c) 2021 Martin Wilck, SUSE LLC
+ * SPDX-License-Identifier: GPL-2.0-or-newer
+ */
+#include <time.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <limits.h>
+#include <assert.h>
+#include "ts-util.h"
+static void ts_random(struct timespec *tv)
+{
+	tv->tv_sec = random() % 2000 - 1000;
+	tv->tv_nsec = random() % LONG_MAX / 2;
+};
+
+static void __attribute__((unused)) ts_print(const struct timespec *tv)
+{
+	fprintf(stderr, "%ld.%06ld ", tv->tv_sec,
+		tv->tv_nsec / (1000000000L / 1000000L));
+};
+
+static int ts_test(void)
+{
+	int i, errors = 0;
+	struct timespec tv[1000], tq[1000];
+	struct timespec *ptv[1000], *qtv[1000];
+	size_t ntv = 0;
+	for (i = 0; i < 1000; i++) {
+		ts_random(&tv[i]);
+	}
+	for (i = 0; i < 1000; i++) {
+		do {
+			tq[i] = tv[i];
+		} while (0);
+	}
+	for (i = 0; i < 1000; i++) {
+		tv[i] = tq[i];
+		qtv[i] = &tq[i];
+		ts_insert(ptv, &ntv, 1000, &tv[i]);
+	}
+	for (i = 0; i < 1000; i++) {
+		ts_normalize(qtv[i]);
+	}
+	ts_sort(qtv, 1000);
+	for (i = 0; i < 1000; i++) {
+		if (i > 0 && ts_compare(ptv[i - 1], ptv[i]) > 0)
+			errors++;
+	}
+	for (i = 0; i < 1000; i++) {
+		if (ts_compare(ptv[i], qtv[i]) != 0)
+			errors++;
+	}
+	return errors;
+};
+
+static int ts_test1(void)
+{
+	int i, errors = 0;
+	struct timespec tv[1000], tq[1000];
+	struct timespec *ptv[1000], *qtv[1000];
+	size_t ntv = 0;
+	for (i = 0; i < 1000; i++) {
+		ts_random(&tv[i]);
+	}
+	for (i = 0; i < 1000; i++) {
+		do {
+			int j = random() % 1000;
+			tq[i] = tv[j];
+		} while (0);
+	}
+	for (i = 0; i < 1000; i++) {
+		tv[i] = tq[i];
+		qtv[i] = &tq[i];
+		ts_insert(ptv, &ntv, 1000, &tv[i]);
+	}
+	for (i = 0; i < 1000; i++) {
+		ts_normalize(qtv[i]);
+	}
+	ts_sort(qtv, 1000);
+	for (i = 0; i < 1000; i++) {
+		if (i > 0 && ts_compare(ptv[i - 1], ptv[i]) > 0)
+			errors++;
+	}
+	for (i = 0; i < 1000; i++) {
+		if (ts_compare(ptv[i], qtv[i]) != 0)
+			errors++;
+	}
+	return errors;
+};;
+
+int main(void)
+{
+	int i, n_err = 0;
+	for (i = 0; i < 1000; i++)
+		n_err += ts_test();
+	for (i = 0; i < 1000; i++)
+		n_err += ts_test1();
+	fprintf(stderr, "TESTS FINISHED, %d errors (#items: %d, #runs: %d)\n",
+		n_err, 1000, 1000);
+	return n_err ? 1 : 0;
+}
---
+++ test/tv-test.c
@@ -0,0 +1,103 @@
+/*
+ * Copyright (c) 2021 Martin Wilck, SUSE LLC
+ * SPDX-License-Identifier: GPL-2.0-or-newer
+ */
+#include <time.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <limits.h>
+#include <assert.h>
+#include "tv-util.h"
+static void tv_random(struct timeval *tv)
+{
+	tv->tv_sec = random() % 2000 - 1000;
+	tv->tv_usec = random() % LONG_MAX / 2;
+};
+
+static void __attribute__((unused)) tv_print(const struct timeval *tv)
+{
+	fprintf(stderr, "%ld.%06ld ", tv->tv_sec,
+		tv->tv_usec / (1000000L / 1000000L));
+};
+
+static int tv_test(void)
+{
+	int i, errors = 0;
+	struct timeval tv[1000], tq[1000];
+	struct timeval *ptv[1000], *qtv[1000];
+	size_t ntv = 0;
+	for (i = 0; i < 1000; i++) {
+		tv_random(&tv[i]);
+	}
+	for (i = 0; i < 1000; i++) {
+		do {
+			tq[i] = tv[i];
+		} while (0);
+	}
+	for (i = 0; i < 1000; i++) {
+		tv[i] = tq[i];
+		qtv[i] = &tq[i];
+		tv_insert(ptv, &ntv, 1000, &tv[i]);
+	}
+	for (i = 0; i < 1000; i++) {
+		tv_normalize(qtv[i]);
+	}
+	tv_sort(qtv, 1000);
+	for (i = 0; i < 1000; i++) {
+		if (i > 0 && tv_compare(ptv[i - 1], ptv[i]) > 0)
+			errors++;
+	}
+	for (i = 0; i < 1000; i++) {
+		if (tv_compare(ptv[i], qtv[i]) != 0)
+			errors++;
+	}
+	return errors;
+};
+
+static int tv_test1(void)
+{
+	int i, errors = 0;
+	struct timeval tv[1000], tq[1000];
+	struct timeval *ptv[1000], *qtv[1000];
+	size_t ntv = 0;
+	for (i = 0; i < 1000; i++) {
+		tv_random(&tv[i]);
+	}
+	for (i = 0; i < 1000; i++) {
+		do {
+			int j = random() % 1000;
+			tq[i] = tv[j];
+		} while (0);
+	}
+	for (i = 0; i < 1000; i++) {
+		tv[i] = tq[i];
+		qtv[i] = &tq[i];
+		tv_insert(ptv, &ntv, 1000, &tv[i]);
+	}
+	for (i = 0; i < 1000; i++) {
+		tv_normalize(qtv[i]);
+	}
+	tv_sort(qtv, 1000);
+	for (i = 0; i < 1000; i++) {
+		if (i > 0 && tv_compare(ptv[i - 1], ptv[i]) > 0)
+			errors++;
+	}
+	for (i = 0; i < 1000; i++) {
+		if (tv_compare(ptv[i], qtv[i]) != 0)
+			errors++;
+	}
+	return errors;
+};;
+
+int main(void)
+{
+	int i, n_err = 0;
+	for (i = 0; i < 1000; i++)
+		n_err += tv_test();
+	for (i = 0; i < 1000; i++)
+		n_err += tv_test1();
+	fprintf(stderr, "TESTS FINISHED, %d errors (#items: %d, #runs: %d)\n",
+		n_err, 1000, 1000);
+	return n_err ? 1 : 0;
+}
